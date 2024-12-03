af093d01aabc33d61f9ab73e3644d3cddf501991
ilazarkov <ilazarkov@gmail.com>
2024-12-02 23:27:11 -0600

    complete phase 1

--- frontend/src/components/LoginFormPage/LoginFormPage.jsx
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,36 +1,61 @@
 import { useState } from "react";
 import { login } from "../../store/session";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import "./LoginForm.css"
+import { Navigate } from "react-router-dom";
 
 export default function LoginFormPage() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
     const dispatch = useDispatch()
+    const sessionUser = useSelector(state => state.session.user)
+    const [credential, setCredential] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState({})
+
+    if (sessionUser) return <Navigate to="/" replace={true} />
 
     const onSubmit = (e) => {
         e.preventDefault()
+        setErrors({})
         dispatch(login({
-            credential: username,
+            credential,
             password
-        }))
-    }
+        })).catch(
+            async (res) => {
+                const data = await res.json();
+                if (data?.errors) setErrors(data.errors);
+            }
+        );
+    };
 
     return (
-        <form >
-            <input
-                type="text"
-                name="username"
-                onChange={(e) => setUsername(e.target.value)} />
-            <input
-                type="password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)} />
-            <button
-                type="submit"
-                onClick={onSubmit}
-            >
-                Login
-            </button>
-        </form>
+        <>
+            <h1>Log In</h1>
+            <form >
+                <div>
+                    <label htmlFor="username">Username or Email</label>
+
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        onChange={(e) => setCredential(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                {errors.credential && <p>{errors.credential}</p>}
+                <button
+                    type="submit"
+                    onClick={onSubmit}
+                >
+                    Login
+                </button>
+            </form>
+        </>
     );
 }
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -9,29 +9,30 @@ const DELETE_SESSION = "session/DELETE_SESSION"
 //action creators
 export const login = (user) => async function (dispatch) {
     const { credential, password } = user
-    try {
-
-        const res = await csrfFetch('/api/session', {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                credential,
-                password
-            })
+    const res = await csrfFetch('/api/session', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            credential,
+            password
         })
-        if (res.ok) {
-            const user = await res.json()
-            dispatch(setUser(user));
-            return res.body
-        }
-        else {
-            console.error("error occurred on login", await res.json().message);
-            dispatch(removeUser());
-        }
-    } catch (error) {
-        console.error(error);
+    })
+    if (res.ok) {
+        const user = await res.json()
+        dispatch(setUser(user));
+        return res.body
+    }
+    else {
+        console.error("error occurred on login", await res.json().message);
+        dispatch(removeUser());
     }
 }
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
 
 function setUser(user) {
     return {
