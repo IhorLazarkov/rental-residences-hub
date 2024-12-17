3fb49a66875c4297187138d9c77ac7c3cddd2df1
ilazarkov <ilazarkov@gmail.com>
2024-12-16 19:32:01 -0600

    fix for preview has no url key

--- frontend/src/components/ManageSpots/ManageSpostPage.jsx
+++ frontend/src/components/ManageSpots/ManageSpostPage.jsx
@@ -33,7 +33,7 @@ export default function ManageSpostPage() {
                     {Spots.length === 0 && <NavLink to="/spots/new">Create a New Spot</NavLink>}
                     <ul id="spots-container">
                         {Spots.map(spot => {
-                            const preview = spot.SpotImages.find(i => i.preview)
+                            const preview = spot.SpotImages.find(i => i.preview) || {url:""}
                             return <div key={spot.id} className="manage-spot-wrapper">
                                 <SpotCard
                                     key={spot.id}
