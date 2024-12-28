f1365086474727487af0c83016611679bbc9b9fa
ilazarkov <ilazarkov@gmail.com>
2024-12-28 14:11:31 -0600

    improved layout of login form

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -39,39 +39,38 @@ export default function LoginFormModal() {
     }
 
     return (
-        <>
-            <form
-                id="login-form"
-                onSubmit={onSubmit}>
-                <h2>Log In</h2>
-                {Object.entries(errors).map(([key, message]) => <span className="error" key={key}>{message}</span>)}
-                <section>
-                    <label htmlFor="username">Username or Email</label>
-                    <input
-                        autoFocus={true}
-                        type="text"
-                        name="username"
-                        id="username"
-                        placeholder="Username or Email"
-                        onChange={(e) => setCredential(e.target.value)} />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)} />
-                    <button
-                        type="submit"
-                        className={credential.length > 4 || password.length > 6
-                            ? "primary"
-                            : "disabled"}
-                    >
-                        Login
-                    </button>
-                </section>
-                <a href="" onClick={onDemoLogin}>Log In as Demo User</a>
-            </form>
-        </>
+        <form
+            id="login-form"
+            onSubmit={onSubmit}
+        >
+            <h2>Log In</h2>
+            {Object.entries(errors).map(([key, message]) => <span className="error" key={key}>{message}</span>)}
+            <section>
+                <label htmlFor="username">Username or Email</label>
+                <input
+                    autoFocus={true}
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Username or Email"
+                    onChange={(e) => setCredential(e.target.value)} />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)} />
+                <button
+                    type="submit"
+                    className={credential.length > 4 || password.length > 6
+                        ? "primary"
+                        : "disabled"}
+                >
+                    Login
+                </button>
+            </section>
+            <a href="" onClick={onDemoLogin}>Log In as Demo User</a>
+        </form>
     );
 }
