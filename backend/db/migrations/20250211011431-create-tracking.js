d8d0852f5b2e25c1c763155e910c401f66619ce3
ilazarkov <ilazarkov@gmail.com>
2025-02-10 21:11:03 -0600

    small fix of lower case of table

--- backend/db/migrations/20250211011431-create-tracking.js
+++ backend/db/migrations/20250211011431-create-tracking.js
@@ -1,6 +1,6 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-const options = { tableName: "Tracking" };
+const options = { tableName: "tracking" };
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
