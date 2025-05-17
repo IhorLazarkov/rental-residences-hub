cb214c96ffbba9858c2c0262212c0a8d1a33ec55
ilazarkov <ilazarkov@gmail.com>
2025-05-17 16:22:01 -0500

    added styling for images with  img-container

--- frontend/src/components/SpotCard/SpotCard.jsx
+++ frontend/src/components/SpotCard/SpotCard.jsx
@@ -1,26 +1,19 @@
 import "./SpotCard.css"
-import { useEffect, useRef, useState } from "react";
 import { IoIosStar } from "react-icons/io";
 import { NavLink } from "react-router-dom";
+import ImgComp from "../SpotDetails/ImgComp";
 
 export default function SpotCard({ spot }) {
 
-    const imgRef = useRef(null)
-    const [isLoading, setIsLoading] = useState(true)
     const { id, previewImage, city, state, price, avgRating, name } = spot
 
     const rating = parseFloat(avgRating) === 0 ? "New" : avgRating
 
     return (
         <NavLink to={`/${id}`} className="spot-card">
-            <img
-                ref={imgRef}
-                src={previewImage}
-                loading="lazy"
-                style={{ opacity: isLoading ? 0 : 1 }}
-                onLoad={() => setIsLoading(false)}
-                alt={`picture of ${name}`}
-                title={name} />
+
+            <ImgComp url={previewImage} />
+
             <div className="spot-card-info">
                 <div className="subcontainer">
                     <span className="address">{city}, {state}</span>
